cdea485a4b1da7bc1cfb2cd6f498b81aa22a4918
nenikitov <nenikitov@mail.ru>
2021-03-23 13:27:02 -0400

    Added docs and updated keyEvent

--- Controller.h
+++ Controller.h
@@ -5,8 +5,22 @@ class Controller
 {
 	public:
 		// Getters
+		/// <summary>
+		/// Gets the movement direction vector.
+		/// </summary>
+		/// <returns>The movement direction vector. X is horizontal axis, Y is vertical. Can be -1 (backward or leftward), 0 (no input) or 1 (forward or rightward).</returns>
 		sf::Vector2i getMovementDirection();
+		/// <summary>
+		/// Gets the look rotation direction
+		/// </summary>
+		/// <returns>The look direction. Can be -1 (left), 0 (no input) or 1 (right).</returns>
 		int getLookDir();
+		/// <summary>
+		/// Updates the controller.
+		/// </summary>
+		/// <param name="key">A key of an event.</param>
+		/// <param name="pressed">If this key is pressed (true) or released (false).</param>
+		void keyEvent(sf::Keyboard::Key key, bool pressed);
 	private:
 		// Player input output
 		sf::Vector2i movementDirection = sf::Vector2i(0, 0);
@@ -19,7 +33,6 @@ class Controller
 		};
 		bool controlKeysPressed[6] = {false, false, false, false, false, false};
 		// Update methods
-		void keyEvent(sf::Keyboard::Key key, bool pressed);
 		void updateControls();
 };
 
