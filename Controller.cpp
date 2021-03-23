09743ac027bbc787dafb8ac5d313a2b57f43f7f1
nenikitov <nenikitov@mail.ru>
2021-03-23 13:06:01 -0400

    Removed constructor

--- Controller.cpp
+++ Controller.cpp
@@ -1,23 +1,6 @@
 
 #include "Controller.h"
 
-Controller::Controller()
-{
-    // Initialize controls
-    this->controls[0] = sf::Keyboard::W;
-    this->controls[1] = sf::Keyboard::S;
-    this->controls[2] = sf::Keyboard::D;
-    this->controls[3] = sf::Keyboard::A;
-    this->controls[4] = sf::Keyboard::Right;
-    this->controls[5] = sf::Keyboard::Left;
-    // Initialize control key pressed
-    for (int i = 0; i < (sizeof(this->controlKeysPressed) / sizeof(this->controlKeysPressed[0])); i++)
-        controlKeysPressed[i] = false;
-    // Initialize movement direction and look input
-    this->movementDirection = sf::Vector2i(0, 0);
-    this->lookDir = 0;
-}
-
 sf::Vector2i Controller::getMovementDirection()
 {
     return this->movementDirection;
--- Controller.h
+++ Controller.h
@@ -4,19 +4,21 @@
 class Controller
 {
 	public:
-		// Constructor
-		Controller();
 		// Getters
 		sf::Vector2i getMovementDirection();
 		int getLookDir();
 	private:
 		// Player input output
-		sf::Vector2i movementDirection;
-		int lookDir;
+		sf::Vector2i movementDirection = sf::Vector2i(0, 0);
+		int lookDir = 0;
 		void keyEvent(sf::Keyboard::Key key, bool pressed);
 		// Controls
-		sf::Keyboard::Key controls[6];
-		bool controlKeysPressed[6];
+		sf::Keyboard::Key controls[6] = 
+		{
+			sf::Keyboard::W, sf::Keyboard::S, sf::Keyboard::D, sf::Keyboard::A, // Movement WASD
+			sf::Keyboard::Right, sf::Keyboard::Left // Looking - Left and Right Arrows
+		};
+		bool controlKeysPressed[6] = {false, false, false, false, false, false};
 		void updateControls();
 };
 
