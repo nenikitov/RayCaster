1abbe884265b0246488d7084505a509c50f64836
nenikitov <nenikitov@mail.ru>
2021-03-25 09:18:48 -0400

    Implemented simple methods

--- Player.cpp
+++ Player.cpp
@@ -1,1 +1,35 @@
 #include "Player.h"
+
+Player::Player(Controller& controller) : controller(controller)
+{
+	// Check if this can be removed
+	this->controller = controller;
+
+	this->circle.setFillColor(this->COLOR);
+	this->line.setFillColor(this->COLOR);
+}
+
+sf::CircleShape Player::getCircleShape()
+{
+	return this->circle;
+}
+
+sf::RectangleShape Player::getRectangle()
+{
+	return this->line;
+}
+
+int Player::getPositionX()
+{
+	return this->positionX;
+}
+
+int Player::getPositionY()
+{
+	return this->positionY;
+}
+
+double Player::getAngle()
+{
+	return this->angle;
+}
--- Player.h
+++ Player.h
@@ -1,16 +1,23 @@
 #pragma once
 #include "Controller.h"
+#include <SFML\Graphics\CircleShape.hpp>
+#include <SFML\Graphics\RectangleShape.hpp>
 
 class Player
 {
 	public:
-		Player(Controller controller);
+		Player(Controller& controller);
+		sf::CircleShape getCircleShape();
+		sf::RectangleShape getRectangle();
 		int getPositionX();
 		int getPositionY();
 		double getAngle();
 		void updatePlayerLocation();
 	private:
-		Controller controller;
+		Controller& controller;
+		sf::CircleShape circle = sf::CircleShape(10.f);
+		sf::RectangleShape line = sf::RectangleShape(sf::Vector2f(10.f, 2.f));
+		const sf::Color COLOR = sf::Color::Cyan;
 		int positionX = 0;
 		int positionY = 0;
 		double angle = 0;
