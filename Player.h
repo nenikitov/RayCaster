5c49db32dec4d4ff4e3b4ed546da8797b720687c
nenikitov <nenikitov@mail.ru>
2021-03-25 08:54:51 -0400

    Added player header file

--- Player.h
+++ Player.h
@@ -5,5 +5,13 @@ class Player
 {
 	public:
 		Player(Controller controller);
+		int getPositionX();
+		int getPositionY();
+		double getAngle();
+		void updatePlayerLocation();
 	private:
+		Controller controller;
+		int positionX = 0;
+		int positionY = 0;
+		double angle = 0;
 };
