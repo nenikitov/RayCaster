0ae6c991e0100710454072b7bb3b8390c9f1c301
nenikitov <nenikitov@mail.ru>
2021-03-25 09:26:17 -0400

    Added docs for implemented player methods

--- Player.cpp
+++ Player.cpp
@@ -1,34 +1,32 @@
 #include "Player.h"
 
-Player::Player(Controller& controller) : controller(controller)
+// Constructor
+Player::Player(Controller& controller, Level& level) : controller(controller), level(level)
 {
 	// Check if this can be removed
 	this->controller = controller;
+	this->level = level;
 
 	this->circle.setFillColor(this->COLOR);
 	this->line.setFillColor(this->COLOR);
 }
-
+// Getters
 sf::CircleShape Player::getCircleShape()
 {
 	return this->circle;
 }
-
 sf::RectangleShape Player::getRectangle()
 {
 	return this->line;
 }
-
 int Player::getPositionX()
 {
 	return this->positionX;
 }
-
 int Player::getPositionY()
 {
 	return this->positionY;
 }
-
 double Player::getAngle()
 {
 	return this->angle;
--- Player.h
+++ Player.h
@@ -2,19 +2,52 @@
 #include "Controller.h"
 #include <SFML\Graphics\CircleShape.hpp>
 #include <SFML\Graphics\RectangleShape.hpp>
+#include "Level.h"
 
 class Player
 {
 	public:
-		Player(Controller& controller);
+		// Constructor
+		/// <summary>
+		/// Initialize the player.
+		/// </summary>
+		/// <param name="controller">The controller that will control the player.</param>
+		/// <param name="level">The level that the player is on.</param>
+		/// <returns>Initialized player.</returns>
+		Player(Controller& controller, Level& level);
+		// Getters
+		/// <summary>
+		/// Gets the circle shape that represents the body of the player.
+		/// </summary>
+		/// <returns>The body circle.</returns>
 		sf::CircleShape getCircleShape();
+		/// <summary>
+		/// Gets the rectangle shape that represents the direction arrow of the player.
+		/// </summary>
+		/// <returns>The direction rectangle.</returns>
 		sf::RectangleShape getRectangle();
+		/// <summary>
+		/// Gets the position of the player along X axis.
+		/// </summary>
+		/// <returns>X position.</returns>
 		int getPositionX();
+		/// <summary>
+		/// Gets the position of the player along Y axis.
+		/// </summary>
+		/// <returns>Y position.</returns>
 		int getPositionY();
+		/// <summary>
+		/// Gets the rotation of the player (in degrees).
+		/// </summary>
+		/// <returns>Rotation.</returns>
 		double getAngle();
+		/// <summary>
+		/// Updates the player from the controls
+		/// </summary>
 		void updatePlayerLocation();
 	private:
 		Controller& controller;
+		Level& level;
 		sf::CircleShape circle = sf::CircleShape(10.f);
 		sf::RectangleShape line = sf::RectangleShape(sf::Vector2f(10.f, 2.f));
 		const sf::Color COLOR = sf::Color::Cyan;
